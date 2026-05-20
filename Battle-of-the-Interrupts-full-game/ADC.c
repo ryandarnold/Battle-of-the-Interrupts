2d1be5111e0f022b2ba151f685e82ef9e46b8f31
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 14:22:24 -0700

    Refactor ADC.c for improved readability and structure

--- Battle-of-the-Interrupts-full-game/ADC.c
+++ Battle-of-the-Interrupts-full-game/ADC.c
@@ -2,8 +2,6 @@
 // Runs on LM4F120/TM4C123
 // Provide functions that initialize ADC0
 // Last Modified: 11/6/2018
-// Student names: change this to your names or look very silly
-// Last modification date: change this to the last modification date or look very silly
 
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
@@ -20,8 +18,8 @@ void ADC_Init(void){
 	uint32_t	delay=0;
 	delay++;
 	volatile uint32_t delay2;
-	SYSCTL_RCGCGPIO_R |= 0x8;      // 1) activate clock for Port D 			***CHANGE TO PD2***  good!
-  //while((SYSCTL_PRGPIO_R&0x8) == 0){}; 													//		***CHANGE TO PD2***  good!
+	SYSCTL_RCGCGPIO_R |= 0x8;  // 1) activate clock for Port D
+  //while((SYSCTL_PRGPIO_R&0x8) == 0){}; 
 	delay2++;
 	delay2++;
 	delay2++;
@@ -29,10 +27,10 @@ void ADC_Init(void){
 	delay2++;
 	delay2++;
 	delay2++;
-  GPIO_PORTD_DIR_R &= ~0x4	;      // 2) make PD2 input  							***CHANGE TO PD2***	 good!
-  GPIO_PORTD_AFSEL_R |= 0x4;     // 3) enable alternate fun on PD2 		***CHANGE TO PD2***  good!
-  GPIO_PORTD_DEN_R &= ~0x4;      // 4) disable digital I/O on PD2 	 	***CHANGE TO PD2***	 good!
-  GPIO_PORTD_AMSEL_R |= 0x4;     // 5) enable analog fun on PD2	    	***CHANGE TO PD2***  good!
+  GPIO_PORTD_DIR_R &= ~0x4	;      // 2) make PD2 input  							
+  GPIO_PORTD_AFSEL_R |= 0x4;     // 3) enable alternate fun on PD2 		
+  GPIO_PORTD_DEN_R &= ~0x4;      // 4) disable digital I/O on PD2 	 
+  GPIO_PORTD_AMSEL_R |= 0x4;     // 5) enable analog fun on PD2
   SYSCTL_RCGCADC_R |= 0x01;       // 6) activate ADC0 
 
   delay2++;
@@ -47,7 +45,7 @@ void ADC_Init(void){
   ADC0_SSPRI_R = 0x0123;          // 8) Seq 3 is highest priority
   ADC0_ACTSS_R &= ~0x0008;        // 9) disable sample sequencer 3
   ADC0_EMUX_R &= ~0xF000;         // 10) seq3 is software trigger
-  ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+5;  // 11) Ain2 (PD2)		***CHANGE TO PD2***  good!
+  ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+5;  // 11) Ain2 (PD2)
   ADC0_SSCTL3_R = 0x0006;         // 12) no TS0 D0, yes IE0 END0
 	//ADC0_SAC_R = 0x06;              //    2^{n-1} hardware averaging
   ADC0_IM_R &= ~0x0008;           // 13) disable SS3 interrupts
